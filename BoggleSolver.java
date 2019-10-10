c91e976971662d990ebc65ba547fee405325de3d
mtanzim <mtanzim@gmail.com>
2019-10-10 17:55:20 +0200

    working solver

--- BoggleSolver.java
+++ BoggleSolver.java
@@ -8,8 +8,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.TrieSET;
 
-import java.util.ArrayList;
-
 public class BoggleSolver {
 
     private TrieSET words = new TrieSET();
@@ -22,55 +20,63 @@ public class BoggleSolver {
         for (String word : dictionary) words.add(word);
     }
 
-    private void dfsBoard(BoggleBoard board, boolean[][] marked, String curWordArr[][],
+    private void dfsBoard(BoggleBoard board, boolean[][] marked,
                           String prevWord, int curX,
-                          int curY) {
-        boolean debug = true;
-        marked[curX][curY] = true;
+                          int curY, TrieSET validWords) {
+        boolean debug = false;
 
-        // curWordArr[curX][curY] = curWord;
-        // if (debug) StdOut.println("Currently at : " + board.getLetter(curX, curY));
+        if (curX > board.rows() - 1) return;
+        if (curY > board.cols() - 1) return;
+        if (curX < 0) return;
+        if (curY < 0) return;
+        if (marked[curX][curY]) return;
+
+        marked[curX][curY] = true;
+        prevWord = prevWord + board.getLetter(curX, curY);
+        if (debug) StdOut.println("Word formed : " + prevWord);
+        // if (debug) StdOut.println("Len of word formed : " + curWord.length());
+        if (words.contains(prevWord) && prevWord.length() > 2) validWords.add(prevWord);
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
                 if (i == 0 && j == 0) continue;
-                if (curX + i > board.rows() - 1) continue;
-                if (curY + j > board.cols() - 1) continue;
-                if (curX + i < 0) continue;
-                if (curY + j < 0) continue;
-                if (marked[curX + i][curY + j]) continue;
-                String curWord = prevWord + board.getLetter(curX, curY);
-                dfsBoard(board, marked, curWordArr, curWord, curX + i, curY + j);
-                if (debug) StdOut.println("Word formed : " + curWord);
-                if (words.contains(curWord) && curWord.length() > 2) StdOut.println("HOLLLA");
-
+                dfsBoard(board, marked, prevWord, curX + i, curY + j, validWords);
             }
         }
+        marked[curX][curY] = false;
+
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        boolean debug = true;
-        if (debug) StdOut.println(board.toString());
-        ArrayList<String> validWords = new ArrayList<String>();
+        boolean debug = false;
 
-        for (int x = 0; x < board.rows(); x++) {
-            for (int y = 0; y < board.cols(); y++) {
+        TrieSET validWords = new TrieSET();
+        if (debug) StdOut.println(board.toString());
+
+        for (int x = 0; x < board.cols(); x++) {
+            for (int y = 0; y < board.rows(); y++) {
                 if (debug) StdOut.println(board.getLetter(x, y));
                 boolean marked[][] = new boolean[board.rows()][board.cols()];
-                String curWordArr[][] = new String[board.rows()][board.cols()];
-                dfsBoard(board, marked, curWordArr, "", x, y);
+                dfsBoard(board, marked, "", x, y, validWords);
             }
 
         }
 
-        // rv.add("Hi");
         return validWords;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
+        int len = word.length();
+        if (len == 3) return 1;
+        if (len == 4) return 1;
+        if (len == 5) return 2;
+        if (len == 6) return 3;
+        if (len == 7) return 5;
+        if (len > 8) return 11;
         return 0;
+
     }
 
     public static void main(String[] args) {
